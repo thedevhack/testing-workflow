b5ca8c61fb1515698a071768d0f58a6b8a96e16c
thedevhack <sharma.dhardev@gmail.com>
2025-12-27 13:28:57 +0530

    first commmit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const server = express();
+
+server.get("/health-check", (req, res) => {
+    res.json({message:"ok!"})
+})
+
+server.listen(3000, () => {
+    console.log("server started")
+})
+
