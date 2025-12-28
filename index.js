35a990cd893e68e56ef6fba24c727ecaa4d4d072
thedevhack <sharma.dhardev@gmail.com>
2025-12-29 00:23:22 +0530

    testing workflow

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const server = express();
 
 server.get("/health-check", (req, res) => {
-    res.json({message:"ok!"})
+    res.json({message:"ok!!!!!!!!!!1!"})
 })
 
 server.listen(3000, () => {
